8125efeea1b1e13e6abce4e3c45aed92e53331d7
Timur Celik <timur.celik@hs-augsburg.de>
2019-04-12 12:57:06 +0200

    changed LoginView to Welcome: FirstName

--- Main.Master.cs
+++ Main.Master.cs
@@ -4,11 +4,38 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.Security;
+using System.Data.SqlClient;
+using System.Configuration;
 
     public partial class Main : System.Web.UI.MasterPage
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            Label firstName = (Label) LoginView1.FindControl("LabelFirstName");
+            firstName.Text = GetUserFirstName();
+        }
 
+        //Gets the firstName of the current user from the users table
+        protected string GetUserFirstName() {
+            string username = Membership.GetUser().UserName;
+            string firstName = "";
+
+            string dbstring = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+
+            //Set up Connection
+            SqlConnection connection = new SqlConnection(dbstring);
+            connection.Open();
+
+            //Build the select String and the SqlCommand
+            string selectString = "SELECT firstName FROM users WHERE viaId=@viaId";
+            SqlCommand selectCommand = new SqlCommand(selectString, connection);
+            selectCommand.Parameters.AddWithValue("@viaId", username);
+
+            firstName = (string) selectCommand.ExecuteScalar();
+
+            connection.Close();
+
+            return firstName;
         }
     }
