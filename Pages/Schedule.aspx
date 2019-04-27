7303f8736c8a31a98c1192994b6ddd7113bad524
Timur Celik <timur.celik@hs-augsburg.de>
2019-04-27 16:36:38 +0200

    Schedule

--- Pages/Schedule.aspx
+++ Pages/Schedule.aspx
@@ -1,7 +1,33 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Main.Master" AutoEventWireup="true" CodeFile="Schedule.aspx.cs" Inherits="Pages_Schedule" %>
 
-<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
-</asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="MainContentPlaceHolder" Runat="Server">
+    <div id="schedule">
+        <h3 style="margin-bottom:10px">Sport Schedule</h3>
+        <asp:DataList ID="DataList1" runat="server" RepeatColumns="7" DataSourceID="SqlDataSource1" OnItemDataBound="DataList1_ItemDataBound" RepeatDirection="Horizontal">           
+            <ItemTemplate>
+                <div id="scheduleItem">
+                    <asp:Label ID="weekdayName" runat="server" CssClass="scheduleWeekday"/>
+                    <asp:Label ID="weekdayLabel" runat="server" Text='<%# Eval("weekday") %>' CssClass="scheduleWeekday"/>    
+                    <hr/>           
+                    <asp:DataList ID="DataList2" runat="server" RepeatColumns="1" OnItemDataBound="DataList2_ItemDataBound">
+                        <ItemTemplate>
+                            <div id="nestedDataList" runat="server" > 
+                                <asp:Label ID="sportNameLabel" runat="server" Text='<%# Eval("sportName") %>'/> <br />
+                                <asp:Label ID="timeLabel" runat="server" Text='<%# Eval("time") %>'/> <br />
+                                <asp:Label ID="placeLabel" runat="server" Text='<%# Eval("place") %>'/> <br />
+                                <asp:Label ID="cancelledLabel" runat="server" Text='<%# Eval("isCancelled") %>' Visible="false" />
+                            </div>
+                            <hr />
+                        </ItemTemplate>
+                    </asp:DataList> 
+                </div>               
+            </ItemTemplate>            
+        </asp:DataList>
+        <!-- DataSource for the Main DataList. We have to group by weekday otherwise we would have the number of days we have courses on this day (I.e. 2 courses on Monday, 29.04.2019 would give us this day two times)  -->
+        <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:ConnectionString %>" SelectCommand="SET DATEFIRST 1
+SELECT CONVERT(VARCHAR(10), [weekday], 104) as weekday FROM [schedule] WHERE weekday &gt;= dateadd(day, 1-datepart(dw, getdate()), CONVERT(date,getdate())) and
+weekday&lt; dateadd(day, 8-datepart(dw, getdate()), CONVERT(date,getdate())) GROUP BY weekday">
+        </asp:SqlDataSource>
+    </div> 
 </asp:Content>
 
--- Pages/Schedule.aspx.cs
+++ Pages/Schedule.aspx.cs
@@ -4,11 +4,73 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Globalization;
+using System.Drawing;
 
 public partial class Pages_Schedule : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+
+    //This Method is called everytime a new Item got bound to the datalist
+    protected void DataList1_ItemDataBound(object sender, DataListItemEventArgs e) {
+        Label weekday = e.Item.FindControl("weekdayLabel") as Label;
+        Label weekdayName = e.Item.FindControl("weekdayName") as Label;
+        //The nested DataList
+        DataList list2 = e.Item.FindControl("DataList2") as DataList;
+        weekdayName.Text = GetWeekdayName(weekday.Text);
+        DataTable dataTable = GetSportsPerWeekday(weekday.Text);
+        list2.DataSource = dataTable;
+        list2.DataBind();
+    }
+
+    //Get all the sports for one weekday
+    protected DataTable GetSportsPerWeekday(string weekday) {
+        //Append the time to the weekday to set it as a parameter for the db query
+        string weekdayParsed = weekday + " 00:00:00";
+        DataTable dataTable = new DataTable();
+        //Gets the default connection string/path to our database from the web.config file
+        string dbstring = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+        
+        //Set up Connection
+        SqlConnection connection = new SqlConnection(dbstring);
+        connection.Open();
+
+        //Build the select String and the SqlCommand
+        string selectString = "SELECT sportName, time, place, isCancelled FROM schedule WHERE weekday=@weekday";
+        SqlCommand selectCommand = new SqlCommand(selectString, connection);
+        //selectCommand.Parameters.AddWithValue("@weekday", weekday);
+        selectCommand.Parameters.AddWithValue("@weekday", DateTime.Parse(weekdayParsed));    
+        SqlDataAdapter adapter = new SqlDataAdapter(selectCommand);
+        adapter.Fill(dataTable);
+
+        //Sort the table by the time of each course
+        dataTable.DefaultView.Sort = "time asc";
+        dataTable = dataTable.DefaultView.ToTable();
+        connection.Close();
+        dataTable.Dispose();
+        return dataTable;
+    }
+
+    //Get the name of the current weekday, binded to the DataList
+    protected string GetWeekdayName(string weekday) {
+        DateTime date = DateTime.Parse(weekday + " 00:00:00");
+        return date.ToString("dddd" , new CultureInfo("en-US"));   
+    }
+
+    protected void DataList2_ItemDataBound(object sender, DataListItemEventArgs e) {
+        Label cancelledLabel = e.Item.FindControl("cancelledLabel") as Label;
+        //If a lesson is cancelled, set the cancelled Label to visible and the color to a dark red
+        if(cancelledLabel.Text == "True") {
+            cancelledLabel.Visible = true;
+            cancelledLabel.Text = "CANCELLED";
+            cancelledLabel.ForeColor = Color.FromArgb(204, 0, 0);
+        }
+        
     }
 }
