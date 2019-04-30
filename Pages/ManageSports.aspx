9b4e115bc0b08cfeeec24df8f5dd14550bafc341
Dudu <maxence.duroy@etudiant.univ-reims.fr>
2019-04-30 17:39:06 +0200

    Beginning of ManageSports, just able to add news to DB

---
+++ Pages/ManageSports.aspx
@@ -0,0 +1,9 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Main.Master" AutoEventWireup="true" CodeFile="ManageSports.aspx.cs" Inherits="Pages_ManageSports" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContentPlaceHolder" Runat="Server">   
+    <asp:DropDownList  runat="server" ID="dropDownSportName"/>
+    <asp:TextBox ID="sendNewsTextBox" runat="server" />
+    <asp:Button ID="sendNews" runat="server" Text="Send" OnClick="sendNews_Click"/> 
+</asp:Content>
---
+++ Pages/ManageSports.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Globalization;
+using System.Drawing;
+
+public partial class Pages_ManageSports : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        string teachername = Helper.Helper.GetUserFirstName();
+        DataTable dataTable = new DataTable();
+        //Gets the default connection string/path to our database from the web.config file
+        string dbstring = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+
+        //Set up Connection
+        SqlConnection connection = new SqlConnection(dbstring);
+        connection.Open();
+
+        //Build the select String and the SqlCommand
+        string selectString = "SELECT sportName FROM sports WHERE teacherName=@teachername";
+        SqlCommand selectCommand = new SqlCommand(selectString, connection);
+        //selectCommand.Parameters.AddWithValue("@weekday", weekday);
+        selectCommand.Parameters.AddWithValue("@teachername", teachername);
+        SqlDataAdapter adapter = new SqlDataAdapter(selectCommand);
+        adapter.Fill(dataTable);
+
+        dropDownSportName.DataSource = dataTable;
+        dropDownSportName.DataBind();
+        dropDownSportName.DataTextField = "sportName";
+        
+        dropDownSportName.DataBind();
+    }
+
+    protected void sendNews_Click(object sender, EventArgs e)
+    {
+
+        String news = sendNewsTextBox.Text;
+        String sportname = dropDownSportName.SelectedValue;
+
+        //Gets the default connection string/path to our database from the web.config file
+        string dbstring = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+
+        //Set up Connection
+        SqlConnection connection = new SqlConnection(dbstring);
+        connection.Open();
+
+        //Build the select String and the SqlCommand
+        string insertString = "INSERT INTO news(sportname,news) VALUES(@sportname,@news)";
+        SqlCommand insertCommand = new SqlCommand(insertString, connection);
+
+        insertCommand.Parameters.AddWithValue("@sportname", sportname);
+        insertCommand.Parameters.AddWithValue("@news", news);
+
+        insertCommand.ExecuteNonQuery();
+
+        connection.Close();
+
+        sendNewsTextBox.Text = "";
+    }
+}
